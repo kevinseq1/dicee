5c1a5e72408b4582009cda6c5526c0ea1e7a25ce
kevinseq1 <kevinseq1@gmail.com>
2022-12-24 18:26:17 -0800

    Add roll button functionality in ViewController.swift ➡️

--- Dicee-iOS13/ViewController.swift
+++ Dicee-iOS13/ViewController.swift
@@ -16,13 +16,18 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         
+        // Do any additional setup after loading the view.
         // Change the UI element to the specified string
         diceImageView1.image = UIImage(imageLiteralResourceName: "DiceSix")
         diceImageView2.image = UIImage(imageLiteralResourceName: "DiceThree")
     }
 
-
+    // Change image when roll button is pressed
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        diceImageView1.image = UIImage(imageLiteralResourceName: "DiceFour")
+        diceImageView2.image = UIImage(imageLiteralResourceName: "DiceFour")
+    }
+    
 }
 
